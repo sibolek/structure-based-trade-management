08e43b3d8be3a81ad2b306d98e91bf6deae98bd4
sibolek <105567480+sibolek@users.noreply.github.com>
2026-08-26 14:14:17 -0600

    Add analytics module index

---
+++ analytics/index.mjs
@@ -0,0 +1,6 @@
+export * from "./execution-metrics.mjs";
+export * from "./management-metrics.mjs";
+export * from "./r-metrics.mjs";
+export * from "./mfe-mae.mjs";
+export * from "./capture-efficiency.mjs";
+export * from "./counterfactuals.mjs";
